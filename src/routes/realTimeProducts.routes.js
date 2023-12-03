1f30612ca0d59b5009c946aca781ceaa5a789d59
Leonardo Tricotti <tricottileo@gmail.com>
2023-12-03 09:24:41 -0300

    commit

--- src/routes/realTimeProducts.routes.js
+++ src/routes/realTimeProducts.routes.js
@@ -18,8 +18,11 @@ router.get("/:pid", getOne);
 
 //Ruta para guardar un producto
 router.post(
+  console.log("router"),
   "/",
+  console.log("router"),
   uploader.fields([{ name: "userProductImage", maxCount: 3 }]),
+  console.log("router"),
   saveProduct
 );
 
