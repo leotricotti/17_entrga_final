e57c1b342814eaa2c6d1a6a916cb1f94a9740188
Leonardo Tricotti <tricottileo@gmail.com>
2023-12-04 12:55:33 -0300

    commit

--- src/controllers/realTimeProducts.controller.js
+++ src/controllers/realTimeProducts.controller.js
@@ -12,6 +12,8 @@ async function saveProduct(req, res, next) {
   // Parsear el producto del cuerpo de la solicitud
   const { title, description, code, price, stock, category, owner } = req.body;
 
+  console.log(category);
+
   try {
     // Verificar que todos los campos requeridos estén presentes
     if (!title || !description || !price || !code || !stock || !category) {
